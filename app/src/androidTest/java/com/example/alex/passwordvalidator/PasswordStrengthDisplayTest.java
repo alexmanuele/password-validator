aba27eaa18a214b493e3f804de1d6ddb2c4b7348
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-06-09 21:51:35 -0300

    Changed name of instrumented test, added Rules to test

--- app/src/androidTest/java/com/example/alex/passwordvalidator/PasswordStrengthDisplayTest.java
+++ app/src/androidTest/java/com/example/alex/passwordvalidator/PasswordStrengthDisplayTest.java
@@ -2,8 +2,10 @@ package com.example.alex.passwordvalidator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -21,7 +23,10 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class PasswordStrengthDisplayTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
