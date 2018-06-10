ce76eca335108ee0020096560da4bb707f9b6e9e
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-06-09 21:32:52 -0300

    Wrote espresso tests to check if UI elements are present, if UI elements serve desired purpose

--- app/src/androidTest/java/com/example/alex/passwordvalidator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/alex/passwordvalidator/ExampleInstrumentedTest.java
@@ -7,6 +7,12 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +28,20 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.alex.passwordvalidator", appContext.getPackageName());
+    }
+    @Test
+    public void elementsExist() throws Exception{
+        onView(withId(R.id.editText)).check(matches(isDisplayed()));
+        onView(withId(R.id.textView)).check(matches(isDisplayed()));
+        onView(withId(R.id.check)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void displayPasswordDescription() throws Exception{
+        onView(withId(R.id.editText)).perform(typeTextIntoFocusedView("elePh@ntSr00l"));
+        onView(withId(R.id.check)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Very Strong")));
+
+
     }
 }
